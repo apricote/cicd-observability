a58f89f8a22cf9afa3fc56ea27ad810fe954d701
Julian Tölle <julian.toelle97@gmail.com>
2024-06-22 22:18:05 +0200

    deploy cluster

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/apricote/cicd-observability/pkg"
 )
 
 func main() {
 	log.Println(pkg.HelloWorld())
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
 }
